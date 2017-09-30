edaf5087cb0ca54f81f9da2de8f86a49e17bfe59
jLaw <jacquelynlaw@gmail.com>
2017-09-30 18:55:16 -0400

    Changes to Set Budgets Page
    Changed layout

--- app/src/main/java/me/huongdang/digitaldream/SetBudgets.java
+++ app/src/main/java/me/huongdang/digitaldream/SetBudgets.java
@@ -2,12 +2,21 @@ package me.huongdang.digitaldream;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class SetBudgets extends AppCompatActivity {
+
+public class SetBudgets extends AppCompatActivity /*extends ActionBarActivity implements View.OnClickListener*/ {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_budgets);
     }
+
+
+    // private Button buttonClick;
+
 }
