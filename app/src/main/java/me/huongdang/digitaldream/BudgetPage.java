5446a14af8377d89dadc327907c5cafea00c6b0d
jLaw <jacquelynlaw@gmail.com>
2017-09-30 11:05:42 -0400

    BudgetPage
    Added activity page for Budget Chart

---
+++ app/src/main/java/me/huongdang/digitaldream/BudgetPage.java
@@ -0,0 +1,15 @@
+package me.huongdang.digitaldream;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BudgetPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budget_page);
+    }
+
+    // Use Microsoft Azure to analyze budget and spending
+}
