62dd94b7ca5e50556c1c1ac60482572390a11581
jLaw <jacquelynlaw@gmail.com>
2017-10-01 12:19:44 -0400

    Added buttons between goals page
    Can change activity page to goals settings

--- app/src/main/java/me/huongdang/digitaldream/MainActivity.java
+++ app/src/main/java/me/huongdang/digitaldream/MainActivity.java
@@ -10,23 +10,38 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button launchBudgetsActivity;
+    private Button launchGoalsActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         launchBudgetsActivity = (Button) findViewById(R.id.button5);
+        launchGoalsActivity = (Button) findViewById(R.id.button8);
 
         launchBudgetsActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                launchActivity();
+                launchBudgetsActivity();
             }
+        });
+
+        launchGoalsActivity.setOnClickListener(new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+                launchGoalsActivity();
+            }
+
         });
     }
 
-    private void launchActivity() {
+    private void launchBudgetsActivity() {
         Intent intent = new Intent(this, SetBudgets.class);
+        startActivity(intent);
+    }
+
+    private void launchGoalsActivity() {
+        Intent intent = new Intent(this, activity_goals_first.class);
         startActivity(intent);
     }
 }
