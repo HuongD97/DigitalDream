81798c60a3a4c764207d734751fbe98c36e3b1ed
Rachael Sera <rsera@knights.ucf.edu>
2017-10-01 12:22:02 -0400

    added avatar background

--- app/src/main/java/me/huongdang/digitaldream/AvatarSettings.java
+++ app/src/main/java/me/huongdang/digitaldream/AvatarSettings.java
@@ -17,14 +17,6 @@ public class AvatarSettings extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         final Button button = (Button) findViewById(R.id.button5);
         button.setOnClickListener(new View.OnClickListener() {
